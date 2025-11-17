f0b48b7211eff3eab6b53b19b790b5d953118541
VarshaMK08 <varsha2006mk@gmail.com>
2025-11-17 21:44:44 +0530

    learnt variable,dataTypes and print function in Dart

---
+++ Day01/day_01.dart
@@ -0,0 +1,34 @@
+// Printing output on the screen
+void main() {
+  print("hello world");
+
+  // Variables and data types
+
+  String name = "Varsha";
+  int age = 19;
+  double height = 5.3;
+  bool isStudent = true;
+
+  print("Name: $name");
+  print("Age: $age");
+  print("Height: $height");
+  print("IsStudent: $isStudent");
+
+  // Var and Dynamic
+
+  var city = "Mysuru";
+  dynamic item = 10;
+  item = true;
+  item = "Hello Dart";
+
+  print("City: $city");
+  print("Item: $item");
+
+  // simple arithmetic
+
+  int a = 10;
+  int b = 20;
+
+  int sum = a + b;
+  print("Sum: $sum");
+}
